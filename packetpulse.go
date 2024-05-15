14d94d708e704bd5db6addbea78585f83133b5db
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-15 16:16:48 -0400

    Added minor update

---
+++ packetpulse.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
+)
+
+func main() {
+	// Find all devices
+	devices, err := pcap.FindAllDevs()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Choose a device to listen on
+	// For example, choose the first device
+	device := devices[0].Name
+
+	// Open the device for packet capture
+	handle, err := pcap.OpenLive(device, 1600, true, pcap.BlockForever)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer handle.Close()
+
+	// Set filter for capturing only TCP packets
+	err = handle.SetBPFFilter("tcp")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Start processing packets
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range packetSource.Packets() {
+		fmt.Println(packet)
+	}
+}
